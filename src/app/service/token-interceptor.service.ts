ed78a9f53fd1e94701d662854f457875cb105416
princekwame1 <princekwame716@gmail.com>
2022-04-28 03:31:37 +0000

    update

--- src/app/service/token-interceptor.service.ts
+++ src/app/service/token-interceptor.service.ts
@@ -21,16 +21,16 @@ export class TokenInterceptorService implements HttpInterceptor {
 
 
        
-      if (!req.headers.has('Content-Type')) {
-          req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
-      }
+    //   if (!req.headers.has('Content-Type')) {
+    //       req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
+    //   }
      
-      if (!req.headers.has('Access-Control-Allow-Origin')) {
-        req = req.clone({ headers: req.headers.set('Access-Control-Allow-Origin', '*') });
-    }
-      if (authService.GetToken()) {
-        req = req.clone({ headers: req.headers.set('Authorization', authService.GetToken()) });
-    }
+    //   if (!req.headers.has('Access-Control-Allow-Origin')) {
+    //     req = req.clone({ headers: req.headers.set('Access-Control-Allow-Origin', '*') });
+    // }
+    //   if (authService.GetToken()) {
+    //     req = req.clone({ headers: req.headers.set('Authorization', authService.GetToken()) });
+    // }
     // });
     return next.handle(req);
   }
